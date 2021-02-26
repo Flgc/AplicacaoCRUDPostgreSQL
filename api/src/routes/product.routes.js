05604e524e0149d47ba3fb0424d2d75473abef77
Fabio Luis <ml@fabiotecnico1.biz>
2021-02-25 21:45:15 -0300

    Criando a rota: (POST) 'Create Product' - (routes e controller

---
+++ api/src/routes/product.routes.js
@@ -0,0 +1,18 @@
+// @ts-nocheck
+/**
+ * Arquivo: src/routes/product.routes.js
+ * Descrição: arquivo responsável pelas rotas da api relacionado a classe 'Product'.
+ * Data: 25/02/2021
+ */
+
+ const router = require('express-promise-router')();
+ const productController = require('../controllers/product.controller');
+
+
+ // ==> Define as rotas do CRUD - 'Product':
+
+ // ==> Rota responsável por criar um novo 'Product': (POST): localhost:3000/api/products
+
+ router.post('/products', productController.createProduct);
+
+ module.exports = router;
